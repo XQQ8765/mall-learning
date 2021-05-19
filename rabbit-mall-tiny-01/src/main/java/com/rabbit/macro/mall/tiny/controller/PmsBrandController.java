1505ee6513e9ef6b8c554f1104c476b20282587d
Rabbit Xiao (rxiao) <Rabbit.Xiao@quest.com>
2021-05-19 15:56:01 +0800

    [rabbit-mall-tiny-01] Add Swgger API document.

--- rabbit-mall-tiny-01/src/main/java/com/rabbit/macro/mall/tiny/controller/PmsBrandController.java
+++ rabbit-mall-tiny-01/src/main/java/com/rabbit/macro/mall/tiny/controller/PmsBrandController.java
@@ -3,6 +3,8 @@ package com.rabbit.macro.mall.tiny.controller;
 import com.rabbit.macro.mall.tiny.common.api.CommonResult;
 import com.rabbit.macro.mall.tiny.mbg.model.PmsBrand;
 import com.rabbit.macro.mall.tiny.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
 @Controller
 @RequestMapping("/r/brand")
 public class PmsBrandController {
@@ -23,12 +26,14 @@ public class PmsBrandController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
 
+    @ApiOperation("获取所有品牌列表")
     @RequestMapping(value = "listAll", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<PmsBrand>> getBrandList() {
         return CommonResult.success(demoService.listAllBrand());
     }
 
+    @ApiOperation("分页查询品牌列表")
     @RequestMapping(value = "list", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<PmsBrand>> listBrand(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
