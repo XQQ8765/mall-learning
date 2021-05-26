1b39162f9691d04079c19e2028a0498807e9ed06
Rabbit Xiao (rxiao) <Rabbit.Xiao@quest.com>
2021-05-26 11:27:55 +0800

    [rabbit-mall-tiny-04] Add OrderTimeOutCancelTask.

---
+++ rabbit-mall-tiny-04/src/main/java/com/rabbit/macro/mall/tiny/config/SpringTaskConfig.java
@@ -0,0 +1,13 @@
+package com.rabbit.macro.mall.tiny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置
+ * https://macrozheng.github.io/mall-learning/#/architect/mall_arch_06
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
